040dc16103cdb9ef6732f389af6b16afb99764d7
vampire2017 <shenjun1989@qq.com>
2017-12-07 19:38:20 +0800

    add test

--- src/test/create_database_main.cpp
+++ src/test/create_database_main.cpp
@@ -1,11 +1,11 @@
 //
 // Created by bpeer on 17-9-14.
 //
-#include "create_database.h"
+#include "../create/create_database.h"
 #include <dirent.h>
-#include "cJSON.h"
+#include "../common/timer.h"
 
-#include "TestService.h"
+#include "CreateDbService.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TServerSocket.h>
@@ -20,15 +20,9 @@ using namespace ::apache::thrift::server;
 
 using boost::shared_ptr;
 
-struct Time
-{
-	uint32_t sec;
-	uint32_t nsec;
-};
-
-class TestServiceHandler : virtual public TestServiceIf{
+class CreateDbServiceHandler : virtual public CreateDbServiceIf{
 public:
-	TestServiceHandler():msaveData(1) {
+	CreateDbServiceHandler():msaveData(1) {
 		// Your initialization goes here
 		std::chrono::steady_clock::time_point t100 = std::chrono::steady_clock::now();
 
@@ -49,16 +43,18 @@ public:
 		msaveData.db = db_t;
 	}
 
-	void test(const std::string& img_, const double odom_x, const double odom_y, const double odom_th) {
+	void createDb(const std::string& img_, const Data& data)
+	{
 		// Your implementation goes here
 		std::vector<uchar> bytesImg( img_.begin(),img_.end());
 		cv::Mat curr_image = cv::imdecode( bytesImg, CV_LOAD_IMAGE_COLOR );
 
 		//@todo here need add time in...*****
 		///input  map2odom
-		msaveData.map2odom_.x = odom_x;
-		msaveData.map2odom_.y = odom_y;
-		msaveData.map2odom_.th = odom_th;
+		msaveData.map2odom_.x = (float) data.x;
+		msaveData.map2odom_.y = (float) data.y;
+		msaveData.map2odom_.th = (float) data.th;
+		msaveData.map2odom_.stamp = data.stamp;
 		msaveData.saveCb( curr_image, msaveData.map2odom_ );
 
 		std::cout << "callback  out..  " << std::endl;
@@ -66,15 +62,15 @@ public:
 		cv::imshow("img_decode", curr_image);
 		test_save_data = cv::waitKey(1);
 		printf("test-- odom: \n");
-		std::cout << "odom_x:" << odom_x
-		          << " odom_y:" << odom_y
-		          << " odom_th:" << odom_th << std::endl;
+		std::cout << "odom_x:" << data.x
+		          << " odom_y:" << data.y
+		          << " odom_th:" << data.th << std::endl;
 
 		//@todo  test save output
 		if(test_save_data == 27)
 		{
-			msaveData.db.save("/home/bpeer/catkin_ws/src/bpeer_sj/database/database_test.db");  //DBoW数据库
-			msaveData.save_file("/home/bpeer/catkin_ws/src/bpeer_sj/database/test_id_locate.txt");  //图片+位姿 数据库
+			msaveData.db.save("/home/bpeer/catkin_ws/src/bpeer_sj/database/database_test_12_07.db");  //DBoW数据库
+			msaveData.save_file("/home/bpeer/catkin_ws/src/bpeer_sj/database/test_id_locate_12_07.txt");  //图片+位姿 数据库
 //			while(1);
 		}
 		///output end
@@ -90,9 +86,9 @@ int main( int argc, char** argv )
 {
 	//@todo here need todo sth  -->  input img & odom;
 	///thrift begin
-	int port = 9060;
-	shared_ptr<TestServiceHandler> handler(new TestServiceHandler());
-	shared_ptr<TProcessor> processor(new TestServiceProcessor(handler));
+	int port = 10087;
+	shared_ptr<CreateDbServiceHandler> handler(new CreateDbServiceHandler());
+	shared_ptr<TProcessor> processor(new CreateDbServiceProcessor(handler));
 	shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
 	shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
 	shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
