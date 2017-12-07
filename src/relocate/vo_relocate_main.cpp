e29e3bc77a5a7df6ca34397fd36bdc9d03fb4030
vampire2017 <shenjun1989@qq.com>
2017-12-07 19:03:24 +0800

    change bin

--- src/relocate/vo_relocate_main.cpp
+++ src/relocate/vo_relocate_main.cpp
@@ -107,8 +107,8 @@ int main(int argc, char **argv) {
 	shared_ptr<TProcessor> processor(new WithReturnServiceProcessor(handler));
 	shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
 	shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-//	shared_ptr<TProtocolFactory> protocolFactory(new TCompactProtocolFactory());
-	shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+	shared_ptr<TProtocolFactory> protocolFactory(new TCompactProtocolFactory());
+//	shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
 	TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
 	server.serve();
